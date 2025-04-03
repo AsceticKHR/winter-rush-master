eb8b0092391dc1eef06b8af190456f734a8db388
Taixi6 <21251878@life.hkbu.edu.hk>
2025-04-03 19:26:02 +0800

    shader impacts

--- src/SuperShader.js
+++ src/SuperShader.js
@@ -22,15 +22,33 @@ THREE.SuperShader = {
 		"hueAmount":  { type: "f", value: 0 }, //0-1
 		"saturation": { type: "f", value: 0 },
 
+		//Phong Reflection
+		"lightDir":   { type: "v3", value: new THREE.Vector3(1.0, 1.0, 1.0) },
+		"lightInt":   { type: "f", value: 1.0 },
+		"Ka":         { type: "f", value: 0.5 },
+		"Kd":         { type: "f", value: 0.3 },
+		"Ks":         { type: "f", value: 0.4 },
+		"shininess":  { type: "f", value: 10.0 },
+
+		//Rim Light
+		"rimStart":   { type: "f", value: 0.1 },
+		"rimEnd":     { type: "f", value: 0.2 },
+		"rimCoef":    { type: "f", value: 0.2 },
+		"rimColor":   { type: "v3", value: new THREE.Vector3(0.4, 0.6, 0.7) },
+
 	},
 
 	vertexShader: [
 
 		"varying vec2 vUv;",
+		"varying vec3 vPosition;",
+		"varying vec3 vNormal;",
 
 		"void main() {",
 
 			"vUv = uv;",
+			"vPosition = position;",
+			"vNormal = normal;",
 			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
 
 		"}"
@@ -51,8 +69,40 @@ THREE.SuperShader = {
 		"uniform float hueAmount;",
 		"uniform float saturation;",
 
+		//Phong Reflection uniforms
+		"uniform vec3 lightDir;",
+		"uniform float lightInt;",
+		"uniform float Ka;",
+		"uniform float Kd;",
+		"uniform float Ks;",
+		"uniform float shininess;",
+
+		//Rim Light uniforms
+		"uniform float rimStart;",
+		"uniform float rimEnd;",
+		"uniform float rimCoef;",
+		"uniform vec3 rimColor;",
 
 		"varying vec2 vUv;",
+		"varying vec3 vPosition;",
+		"varying vec3 vNormal;",
+
+		"vec2 PhongDir(vec3 lightDir, float lightInt, float Ka, float Kd, float Ks, float shininess) {",
+			"vec3 s = normalize(lightDir);",
+			"vec3 v = normalize(-vPosition);",
+			"vec3 n = normalize(vNormal);",
+			"vec3 h = normalize(2.0*n*n*s - s);",
+			"float diffuse = Ka + Kd * lightInt * max(0.0, dot(n, s));",
+			"float spec = Ks * pow(max(0.0, dot(v,h)), shininess);",
+			"return vec2(diffuse, spec);",
+		"}",
+
+		"vec3 rim(vec3 color, float start, float end, float coef) {",
+			"vec3 normal = normalize(vNormal);",
+			"vec3 eye = normalize(-vPosition);",
+			"float rim = smoothstep(start, end, 1.0 - dot(normal, eye));",
+			"return clamp(rim, 0.0, 1.0) * coef * color;",
+		"}",
 
 		"void main() {",
 
@@ -79,6 +129,16 @@ THREE.SuperShader = {
 
 
  			"col = vec4( mix( col.rgb, shiftedCol.rgb, hueAmount ), 1.0 );",
+
+			// Apply Phong reflection
+			"vec3 lightVec = lightDir - vPosition;",
+			"vec2 ds = PhongDir(lightVec, lightInt, Ka, Kd, Ks, shininess);",
+			"float phongBrightness = ds.x + ds.y;",
+			"col.rgb *= phongBrightness;",
+
+			//Apply Rim Light
+			"vec3 rimLight = rim(rimColor, rimStart, rimEnd, rimCoef);",
+			"col.rgb += rimLight;",
 
 			"gl_FragColor = col;",
 
